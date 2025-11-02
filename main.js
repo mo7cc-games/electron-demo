5ced7f1c25716373a4fbc3c6bf76970267cae075
墨七 <meichangliang@outlook.com>
2025-11-03 05:37:42 +0800

    添加项目

---
+++ main.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // 允许在渲染进程使用 Node API
+    },
+  });
+
+  win.loadFile("index.html"); // 加载本地页面
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
