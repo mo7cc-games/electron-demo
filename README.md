5494d1df2bcb6250522d20a89b1711474d0c3dc8
墨七 <meichangliang@outlook.com>
2025-11-03 06:09:29 +0800

    透明无边框窗口搞定

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # electron-demo
 
-electron demo
+使用 electron 开发的 demo
+
+## 运行
 
 ```bash
 
--- main.js
+++ main.js
@@ -4,6 +4,9 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    frame: false, // 去掉系统边框
+    transparent: true, // 背景透明
+    resizable: true, // 是否允许缩放
     webPreferences: {
       nodeIntegration: true, // 允许在渲染进程使用 Node API
     },
