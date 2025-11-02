3529a5e6e72d0fbad0185124890705108095ad2c
墨七 <meichangliang@outlook.com>
2025-11-03 06:40:59 +0800

    托图标

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-import { app, Menu, Tray, BrowserWindow, ipcMain, nativeImage } from 'electron';
+import { app, Menu, Tray, BrowserWindow, ipcMain } from 'electron';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
+// 获取当前目录
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+const iconPath = path.join(__dirname, './assets/appicon.png');
 
 let win = null;
 let tray = null;
@@ -15,16 +18,14 @@ function createWindow() {
     frame: false, // 去掉系统边框
     transparent: true, // 背景透明
     resizable: true, // 是否允许缩放
+    icon: iconPath,
     webPreferences: {
       nodeIntegration: true, // 允许在渲染进程使用 Node API
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, './app/preload.js'),
     },
   });
 
   win.loadFile('index.html'); // 加载本地页面
-
-  // 创建托盘图标：优先使用项目根目录下的 appicon.png，若不存在则使用一个最小的透明 PNG 数据作为回退
-  const iconPath = path.join(__dirname, 'appicon.png');
 
   tray = new Tray(iconPath);
   // 定义菜单
